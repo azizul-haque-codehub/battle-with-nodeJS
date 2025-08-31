299c5947b11c17c1b61f088ae5fb54d527f1ee90
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-08-31 13:03:37 +0600

    CommonJS Module system

---
+++ 04-Fundamentals_of_NodeJS/chapter-01-Module System/1.2-CommonJS Module System/app.js
@@ -0,0 +1,3 @@
+const utils =  require("./module")
+console.log(utils);
+const {num2,num3} =utils
---
+++ 04-Fundamentals_of_NodeJS/chapter-01-Module System/1.2-CommonJS Module System/module.js
@@ -0,0 +1,5 @@
+const num = "I am from module"
+exports.num = num
+const num2 = "1e1000"
+const num3 = '1e4020'
+exports.module = { num2,num3}
