92d349647c614d3be336f63490991e406dbeed8a
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-09-01 18:31:07 +0600

    es5 module systems if we are not use type=module we can use .mjs extension

--- 04-Fundamentals_of_NodeJS/chapter-01-Module System/2-ES6 Module System/app.js
+++
@@ -1,5 +0,0 @@
-
-console.log("ES6 Module System");
-console.log(module);
-import defltX,{sum,} from './mathModule.js';
- console.log(defltX);
---
+++ 04-Fundamentals_of_NodeJS/chapter-01-Module System/2-ES6 Module System/app.mjs
@@ -0,0 +1,5 @@
+
+console.log("ES6 Module System");
+ 
+import defltX,{sum,} from './mathModule.mjs';
+ console.log(defltX);
