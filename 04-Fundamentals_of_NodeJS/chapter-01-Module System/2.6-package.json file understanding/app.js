3987defd215077e3945a0af693ffa924665360a7
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-09-02 22:11:56 +0600

    understood what's the usage of package.json

---
+++ 04-Fundamentals_of_NodeJS/chapter-01-Module System/2.6-package.json file understanding/app.js
@@ -0,0 +1,13 @@
+console.log("hi hter");
+
+/* "1.0.0" -> its called semantic versioning 
+1st portion 1 = Major
+2nd portion 0 = Minor
+3rd portion 0 = Patch
+
+"^1.0.0"  ^ = carret, its usage for the version can up for Minor or Patch not major
+"~1.0.0" ~ = tilde, tilde usage for the version can up only Patch
+">1.0.0" > = greater than , usage for the version can up which is latest
+"<1.0.0" < = less than , usage for the version can less to current
+
+*/
