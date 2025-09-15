87eb80cf302ce06084479441ffb0ea00bd991432
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-09-15 11:14:01 +0600

    Typed Arrays for read & write on Arraybuffer like an array

---
+++ 06: Mastering Buffers - Handling Binary Data in Node.js/05-TypedArray/app.js
@@ -0,0 +1,30 @@
+// TypedArrays always use the platform's native byte order.
+
+/* Below that's all way to pass argument in TypedArrays
+
+new Uint8Array()
+new Uint8Array(length)
+new Uint8Array(typedArray)
+new Uint8Array(array)
+
+new Uint8Array(buffer)
+new Uint8Array(buffer, byteOffset)
+new Uint8Array(buffer, byteOffset, length)
+
+*/
+
+// const uInt8 = new Uint8Array() // []
+
+// const buf1 = new ArrayBuffer(8)
+// const uInt8 = new Uint8Array(buf1)
+
+// const uInt8 = new Uint8Array(8)
+
+// const uInt8 = new Uint8Array([0xfe,0xab,33,23])
+
+const uInt8 = new Uint8Array(1.99 * 1024 * 1024 * 1024);
+uInt8[0] = 0xff
+uInt8[1] = 0xfe
+uInt8[2] = 0xfd
+
+console.log(uInt8);
---
+++ 06: Mastering Buffers - Handling Binary Data in Node.js/05-TypedArray/note.md
@@ -0,0 +1,6 @@
+### TypedArrays Catelog
+
+1. Int8Array()
+2. UInt8Array()
+3. Int16Array()
+4. UInt16Array()
