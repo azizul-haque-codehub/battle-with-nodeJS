113b9be8872d1889c3076f78066f7f12b8ef810f
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-09-15 15:08:17 +0600

    Transfering data RAM to Network

--- 06: Mastering Buffers - Handling Binary Data in Node.js/06-Transferring Array Buffer Data from Memory to Disk and Network/http-server/client/app.js
+++ 06: Mastering Buffers - Handling Binary Data in Node.js/06-Transferring Array Buffer Data from Memory to Disk and Network/http-server/client/app.js
@@ -1,14 +1,14 @@
-// fetch("http://localhost:3000")
-// .then(res => res.text())
-// .then(data => {
-//     document.querySelector("h1").innerText = data
-//     console.log(data);
-// })
-
 fetch("http://localhost:3000")
-.then(res => res.arrayBuffer())
+.then(res => res.text())
 .then(data => {
-    data = new TextDecoder().decode(data)
     document.querySelector("h1").innerText = data
     console.log(data);
 })
+
+// fetch("http://localhost:3000")
+// .then(res => res.arrayBuffer())
+// .then(data => {
+//     data = new TextDecoder().decode(data)
+//     document.querySelector("h1").innerText = data
+//     console.log(data);
+// })
