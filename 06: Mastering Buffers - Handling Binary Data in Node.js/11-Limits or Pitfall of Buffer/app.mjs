89963698952ad93d729b98cd2aea239df9438940
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-09-17 18:22:43 +0600

    Limition/Pitfall of Buffer

---
+++ 06: Mastering Buffers - Handling Binary Data in Node.js/11-Limits or Pitfall of Buffer/app.mjs
@@ -0,0 +1,12 @@
+
+import {readFile} from 'fs/promises';
+// const a = await readFile("./app.mjs")
+const a = await readFile("/home/azizul-haque/Music/🔥 Palestinian Dabke 🇵🇸 ｜ أقوى عرض دبكة فلسطينية [XGM13IsOBj4].webm")
+const b = await readFile("/home/azizul-haque/Music/🔥 Palestinian Dabke 🇵🇸 ｜ أقوى عرض دبكة فلسطينية [XGM13IsOBj4].webm")
+const c = await readFile("/home/azizul-haque/Music/🔥 Palestinian Dabke 🇵🇸 ｜ أقوى عرض دبكة فلسطينية [XGM13IsOBj4].webm")
+const d = await readFile("/home/azizul-haque/Music/🔥 Palestinian Dabke 🇵🇸 ｜ أقوى عرض دبكة فلسطينية [XGM13IsOBj4].webm")
+
+console.log(a);
+((a,b,c,d)=>{})(a,b,)
+
+console.log("END");
