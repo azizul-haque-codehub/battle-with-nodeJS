02189e6e7a26f6c9a447ec3c4af41b3c0213a13a
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-09-30 21:29:04 +0600

    write 1 lakh number faster than stream

---
+++ 08: Node.js Streams - Handling Data Flow like a Pro/Working with Streams using Promises/app.js
@@ -0,0 +1,16 @@
+import { spawn } from "child_process";
+import fsProms from "fs/promises";
+
+const fileHandle = await fsProms.open("./chapter.txt", "r");
+const readStream = fileHandle.createReadStream({ highWaterMark: 4 })
+readStream.setEncoding("utf8")
+// readStream.on("data",(chunk)=>{
+//     console.log(chunk);
+// })
+const fileHandle2 = await fsProms.open("./chapter..txt","w")
+const writeStream = fileHandle2.createWriteStream()
+const status =  readStream.pipe(writeStream)
+if(!status.flush){
+    console.log("Done");
+}
+ 
