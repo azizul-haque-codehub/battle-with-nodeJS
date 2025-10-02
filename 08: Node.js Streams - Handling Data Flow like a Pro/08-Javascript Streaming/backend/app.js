2270af7f7921ac2f52db4987eee1f9b99572b5a3
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-10-02 19:32:05 +0600

    js stream

--- 08: Node.js Streams - Handling Data Flow like a Pro/08-Javascript Streaming/backend/app.js
+++ 08: Node.js Streams - Handling Data Flow like a Pro/08-Javascript Streaming/backend/app.js
@@ -2,18 +2,16 @@ import http from "http";
 import fs from "fs/promises";
 
 const server = http.createServer(async (req, res) => {
-  res.setHeader("access-control-allow-origin", "*");
-  res.setHeader("Content-Type", "video/mp4");
-  res.setHeader("Content-Disposition", "attachment; filename=streams.mp4");
+  const fileHandle = await fs.open("./river.webp")
+  // const fileHandle = await fs.open("/home/azizul-haque/Amorf.webm");
+  const stats = await fileHandle.stat();
+  const readStream = fileHandle.createReadStream({highWaterMark: 10000} );
 
-  const fileHandle = await fs.open(
-    "C:\\Users\\anura\\OneDrive\\Desktop\\streams.mp4"
-  );
-  const { size } = await fileHandle.stat();
-  res.setHeader("Content-Length", size);
-  const readStream = fileHandle.createReadStream({
-    highWaterMark: 10 * 1024 * 1024,
-  });
+  res.setHeader("access-control-allow-origin", "*");
+  res.setHeader("Content-Type", "text/txt");
+  // res.setHeader("Content-Type", "video/mp4");
+  // res.setHeader("Content-Length", stats.size);
+  // res.setHeader("Content-Disposition", `attachment; filename=${Math.random()}.mp4`);
 
   readStream.on("data", (chunk) => {
     res.write(chunk);
